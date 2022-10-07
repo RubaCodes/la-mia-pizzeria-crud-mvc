df8d5776efc03bc65b7ee5e867d825ffef6e0a45
RubaCodes <rubacodes@gmail.com>
2022-10-07 14:36:28 +0100

    creatzione iniziale modello di appoggio

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace la_mia_pizzeria_static.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Models/categoryPizzas.cs
@@ -0,0 +1,13 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class categoryPizzas
+    {
+        public Pizza Pizza { get; set; }
+        public List<Category> Categories { get; set; }
+        
+        public categoryPizzas() {
+            Pizza = new Pizza();
+        }
+    }
+
+}
